478b4a5bbfc1173fd5101a97a18371ad1074d1a4
Christian Gregg <c_arlt@hotmail.com>
2016-11-11 13:57:18 +0000

    Validate that a User can only join a group once

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,4 +1,6 @@
 class UserGroup < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
+
+  validates :group_id, uniqueness: { scope: :user_id }
 end
--- spec/models/user_group_spec.rb
+++ spec/models/user_group_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe UserGroup, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:group) }
+  it { should validate_uniqueness_of(:group_id).scoped_to(:user_id) }
 end
