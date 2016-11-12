07e80938fd11382eeeca06d4ae700b97f3b62add
Christian Gregg <c_arlt@hotmail.com>
2016-11-12 14:57:04 +0000

    Update to codeclimate 1.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+
+require 'simplecov'
+SimpleCov.start
 
 require 'factory_girl_rails'
 RSpec.configure do |config|
