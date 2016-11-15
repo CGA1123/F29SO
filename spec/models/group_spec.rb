569e772d7fd84152ffff5684ee774183a18c7d3c
Christian Gregg <c_arlt@hotmail.com>
2016-11-15 11:13:15 +0000

    Explain reason for unused variable `_permission3`

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe Group, type: :model do
     it 'returns names of all permissions held by group' do
       permission1 = FactoryGirl.create(:permission)
       permission2 = FactoryGirl.create(:permission)
+      # _permission3 is not used, however we need to create it to check that
+      # Group#permission_strings only return permissions that are assigned to
+      # the Group, as opposed to simply returning all permissions in the
+      # database.
       _permission3 = FactoryGirl.create(:permission)
       subject.permissions << [permission1, permission2]
 
