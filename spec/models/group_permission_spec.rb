504ac6d99d29e109fb7ea6f56eb19501c9f3be26
Christian Gregg <c_arlt@hotmail.com>
2016-11-15 17:01:28 +0000

    Fix issues flagged up by rubocop-rspec

--- spec/models/group_permission_spec.rb
+++ spec/models/group_permission_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe GroupPermission, type: :model do
-  it { should belong_to(:group) }
-  it { should belong_to(:permission) }
-  it { should validate_uniqueness_of(:permission_id).scoped_to(:group_id) }
+  it { is_expected.to belong_to(:group) }
+  it { is_expected.to belong_to(:permission) }
+  it do
+    is_expected.to validate_uniqueness_of(:permission_id).scoped_to(:group_id)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many(:user_groups) }
-  it { should have_many(:groups).through(:user_groups) }
-  it { should validate_presence_of(:groups) }
+  it { is_expected.to have_many(:user_groups) }
+  it { is_expected.to have_many(:groups).through(:user_groups) }
+  it { is_expected.to validate_presence_of(:groups) }
 
-  subject { FactoryGirl.create(:user) }
+  subject(:user) { FactoryGirl.create(:user) }
 
-  let(:group) { subject.groups.first }
+  let(:group) { user.groups.first }
+  let(:other_group) { FactoryGirl.create(:group) }
   let(:permission) { FactoryGirl.create(:permission) }
+  let(:permission2) { FactoryGirl.create(:permission) }
+  let(:permission3) { FactoryGirl.create(:permission) }
 
   describe '#permission?' do
     it 'returns true if User has the relevant permission' do
       group.permissions << permission
-      expect(subject.permission?(permission.name)).to be_truthy
+      expect(user.permission?(permission.name)).to be_truthy
     end
 
     it 'returns false if User does not have the relevant permission' do
-      expect(subject.permission?('non.existent.permission')).to be_falsy
+      expect(user.permission?('non.existent.permission')).to be_falsy
     end
   end
 
   describe '#permission_strings' do
     it 'returns names of permission held by user' do
-      other_group = FactoryGirl.create(:group)
-      permission2 = FactoryGirl.create(:permission)
-      permission3 = FactoryGirl.create(:permission)
       group.permissions << [permission, permission2]
       other_group.permissions << permission3
-      subject.groups << other_group
-
-      expect(subject.permission_strings)
+      user.groups << other_group
+      expect(user.permission_strings)
         .to(match_array([permission.name, permission2.name, permission3.name]))
     end
   end
