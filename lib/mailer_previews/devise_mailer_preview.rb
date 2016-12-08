d23d8727f7fe86f7820ebe7445f45c726826204e
Christian Gregg <c_arlt@hotmail.com>
2016-12-08 15:31:14 +0000

    Set up ActionMailer previewing for `Devise::Mailer`

---
+++ lib/mailer_previews/devise_mailer_preview.rb
@@ -0,0 +1,13 @@
+class Devise::MailerPreview < ActionMailer::Preview
+  def confirmation_instructions
+    Devise::Mailer.confirmation_instructions(User.first, 'token_token')
+  end
+
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(User.first, 'token_token')
+  end
+
+  def password_change
+    Devise::Mailer.password_change(User.first)
+  end
+end
