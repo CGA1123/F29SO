5b90561fcfcd88a8574daa4a10672607e50fbcf8
Christian Gregg <c_arlt@hotmail.com>
2016-12-08 16:26:20 +0000

    Appease rubocop

--- lib/mailer_previews/devise_mailer_preview.rb
+++ lib/mailer_previews/devise_mailer_preview.rb
@@ -1,13 +1,15 @@
-class Devise::MailerPreview < ActionMailer::Preview
-  def confirmation_instructions
-    Devise::Mailer.confirmation_instructions(User.first, 'token_token')
-  end
+module Devise
+  class MailerPreview < ActionMailer::Preview
+    def confirmation_instructions
+      Devise::Mailer.confirmation_instructions(User.first, 'token_token')
+    end
 
-  def reset_password_instructions
-    Devise::Mailer.reset_password_instructions(User.first, 'token_token')
-  end
+    def reset_password_instructions
+      Devise::Mailer.reset_password_instructions(User.first, 'token_token')
+    end
 
-  def password_change
-    Devise::Mailer.password_change(User.first)
+    def password_change
+      Devise::Mailer.password_change(User.first)
+    end
   end
 end
