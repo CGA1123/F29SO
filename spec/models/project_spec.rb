78bc79f8247fd6b3249483306f420a14a6f1b079
Christian Gregg <c_arlt@hotmail.com>
2016-12-23 16:41:02 +0100

    Add relationship between `Project` & `User`, through `ProjectGroup`

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Project, type: :model do
   it { is_expected.to validate_presence_of(:code) }
   it { is_expected.to validate_uniqueness_of(:code).case_insensitive }
   it { is_expected.to have_many(:project_groups) }
+  it { is_expected.to have_many(:users).through(:project_groups) }
 end
