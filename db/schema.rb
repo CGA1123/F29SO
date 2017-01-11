9f867fba9dbce5286432a7680c1662be9232b9f7
Niels Levin <ndl30@hw.ac.uk>
2017-01-11 18:25:07 +0000

    Updated schema via db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111132751) do
+ActiveRecord::Schema.define(version: 20170111154151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,13 @@ ActiveRecord::Schema.define(version: 20170111132751) do
   end
 
   add_index "project_groups", ["project_id"], name: "index_project_groups_on_project_id", using: :btree
+
+  create_table "project_types", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
