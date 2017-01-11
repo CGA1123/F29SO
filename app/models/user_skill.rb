6625eeb0c9f15926173ada05aeecad1839e9d2e3
Niels Levin <ndl30@hw.ac.uk>
2017-01-11 14:18:37 +0000

    Fixed error with using array for enum instead of hash

--- app/models/user_skill.rb
+++ app/models/user_skill.rb
@@ -2,7 +2,7 @@ class UserSkill < ActiveRecord::Base
   belongs_to :user
   belongs_to :skill
 
-  enum rating: [basic: 0, novice: 1, intermediate: 2, advanced: 3, expert: 4]
+  enum rating: { basic: 0, novice: 1, intermediate: 2, advanced: 3, expert: 4 }
 
   validates :skill_id, uniqueness: { scope: :user_id }
   validates :user, :skill, presence: true
