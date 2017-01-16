54b4184073d74a0bb672433c4dc782b4f7a397e5
NielsLevin <ndl30@hw.ac.uk>
2017-01-16 22:07:07 +0000

    Add initial skill controls to profile_controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -45,4 +45,47 @@ class ProfilesController < ApplicationController
   def check_permission
     redirect_to profile_path(@user), alert: 'Nope...' unless edit?(@user)
   end
+
+  # section below for altering user's skills
+  # need to check everything written here
+  def skill_index;
+    # get all skills for this user
+    @skills = UserSkill.find_by(user: params[:user])
+  end
+
+  def skill_add;
+    # add a skill to user's skill list
+    @skill = UserSkill.new(skill_params)
+    if @skill.save
+      # success
+    else
+      # failure
+    end
+  end
+
+  def skill_edit;
+    # update a skill from user's skill list
+    @skill = UserSkill.where(user: params[:user], skill: params[:skill])
+    @skill.rating = # updated rating
+    if @skill.save
+      # success
+    else
+      # failure
+    end
+
+  end
+
+  def skill_remove;
+    # remove a skill from user's skill list
+    @skill = UserSkill.where(user: params[:user], skill: params[:skill])
+    if @skill.destroy
+      # success
+    else
+      # failure
+    end
+  end
+
+  def skill_params;
+    params.require(:user_skill).permit(:user_id, :skill_id, :rating)
+  end
 end
