1d634c30696d07702d0d62cc48adb6d0bb4ab6ef
Niels Levin <ndl30@hw.ac.uk>
2017-01-17 11:43:32 +0000

    Create own controller for userskills editing

---
+++ app/controllers/user_skill_controller.rb
@@ -0,0 +1,45 @@
+class UserSkillController < ApplicationController
+
+  def skill_index;
+    # get all skills for this user
+    @skills = UserSkill.find_by(user: params[:user])
+  end
+
+  def skill_add;
+    # add a skill to user's skill list
+    @skill = UserSkill.new(skill_params)
+    if @skill.save
+      # success
+    else
+      # failure
+    end
+  end
+
+  def skill_edit;
+    # update a skill from user's skill list
+    @skill = UserSkill.where(user: params[:user], skill: params[:skill])
+    @skill.rating = # updated rating
+    if @skill.save
+      # success
+    else
+      # failure
+    end
+
+  end
+
+  def skill_remove;
+    # remove a skill from user's skill list
+    @skill = UserSkill.where(user: params[:user], skill: params[:skill])
+    if @skill.destroy
+      # success
+    else
+      # failure
+    end
+  end
+
+  private
+
+  def skill_params;
+    params.require(:user_skill).permit(:user_id, :skill_id, :rating)
+  end
+end
