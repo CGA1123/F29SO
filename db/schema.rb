fd14418f9de22d793615b7351a8c96dc73849d06
Christian Gregg <c_arlt@hotmail.com>
2017-01-25 17:20:49 +0000

    Add explicit foreign keys to database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125164005) do
+ActiveRecord::Schema.define(version: 20170125170821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -191,4 +191,23 @@ ActiveRecord::Schema.define(version: 20170125164005) do
   add_index "users", ["location_id"], name: "index_users_on_location_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "group_permissions", "groups"
+  add_foreign_key "group_permissions", "permissions"
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
+  add_foreign_key "invitation_groups", "groups"
+  add_foreign_key "invitation_groups", "invitations"
+  add_foreign_key "invitations", "users", column: "inviter_id"
+  add_foreign_key "project_group_permissions", "permissions"
+  add_foreign_key "project_group_permissions", "project_groups"
+  add_foreign_key "project_group_users", "project_groups"
+  add_foreign_key "project_group_users", "users"
+  add_foreign_key "project_groups", "projects"
+  add_foreign_key "project_skills", "projects"
+  add_foreign_key "project_skills", "skills"
+  add_foreign_key "projects", "project_types"
+  add_foreign_key "skills", "skill_types"
+  add_foreign_key "user_skills", "skills"
+  add_foreign_key "user_skills", "users"
+  add_foreign_key "users", "locations"
 end
