213c630a8bb2ce91cbcc303669e62700f29bba36
JAPTaylor <jat1@hw.ac.uk>
2017-02-01 22:48:33 +0000

    Minor Fixes
    Addressed some problems that were pointed out.
    Got index test working (I think)

--- app/controllers/project_groups_controller.rb
+++ app/controllers/project_groups_controller.rb
@@ -1,46 +1,30 @@
 class ProjectGroupsController < ApplicationController
   before_action :set_project, only: [:index]
+
   def index
-    @projectGroups = ProjectGroup.where(project: @project)
-  end
-
-  def create
-    @projectGroup = ProjectGroup.new
-    @projectGroups = ProjectGroups.where(project: @group.project_id)
-
-    @projectGroup.save
-
-    render :index
-  end
-
-  def show; end
-
-  def edit; end
-
-  def update
-    if @projectGroup.update(project_group_params)
-      redirect_to project_path(@project.reload.code)
-    else
-      @project.reload
-      render :edit
-    end
+    @project_groups = ProjectGroup.where(project: @project)
   end
 
   private
 
-  def check_permission(permission)
+  def check_permissions(permission)
     user = current_user
     redirect_to projects_path, alert: 'You do not have permission to perform this action.' unless \
-    user.permission?("projects.#{permission}") ||
+    user.permission?("project.groups.#{permission}") ||
     user.permission?("#{project_id}.projects.groups.#{permission}")
   end
 
   def project_group_params
-    params.require(:project).permit(:name)
+    params.require(:project).permit(:name, :description)
   end
 
   def set_project
     @project = Project.find_by(code: params[:code])
     redirect_to projects_path, alert: 'Project not found' unless @project
+  end
+
+  def set_project_groups
+    @project_group = ProjectGroup.find_by(code: params[:code])
+    redirect_to group_projects_path, alert: "Project Group not found" unless @projectGroup
   end
 end
