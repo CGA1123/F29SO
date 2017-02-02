e16ac2f28914eb3992d5c459bebf6a7e2a77dd70
Christian Gregg <c_arlt@hotmail.com>
2017-02-02 13:01:46 +0000

    Implement a `set_group` before_action

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,7 +1,20 @@
 class GroupUsersController < ApplicationController
+  before_action :set_group
+
   def index; end
 
-  def create; end
+  def create
+    redirect_to group_users_path
+  end
 
-  def destroy; end
+  def destroy
+    redirect_to group_users_path
+  end
+
+  private
+
+  def set_group
+    @group = Group.find_by(name: params[:name])
+    redirect_to groups_path, alert: 'Group not found.' unless @group
+  end
 end
--- spec/controllers/group_users_controller_spec.rb
+++ spec/controllers/group_users_controller_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe GroupUsersController, type: :controller do
-  describe 'GET #index'
-  describe 'POST #create'
-  describe 'DELETE #destroy'
+  let(:group) { FactoryGirl.create(:group) }
+  let(:user) { FactoryGirl.create(:user) }
+
+  before { sign_in user }
+
+  describe 'GET #index' do
+    before { get :index, name: group.name }
+
+    it { expect(response).to be_success }
+
+    it 'sets @group' do
+      expect(assigns[:group]).to eq(group)
+    end
+  end
+
+  describe 'POST #create' do
+    before { post :create, name: group.name }
+
+    it { expect(response).to be_redirect }
+
+    it 'sets @group' do
+      expect(assigns[:group]).to eq(group)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before { delete :destroy, name: group.name }
+
+    it { expect(response).to be_redirect }
+
+    it 'sets @group' do
+      expect(assigns[:group]).to eq(group)
+    end
+  end
 end
