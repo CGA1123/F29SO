2bd47cbdf025c011077dea2bb695e6c307aecf64
Christian Gregg <c_arlt@hotmail.com>
2017-02-03 18:53:15 +0000

    Implement `GroupUsersController#destroy`

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,5 +1,6 @@
 class GroupUsersController < ApplicationController
   before_action :set_group
+  before_action :set_group_user, only: [:destroy]
 
   def index
     @group_users = GroupUser.where(group: @group)
@@ -10,7 +11,18 @@ class GroupUsersController < ApplicationController
   end
 
   def destroy
-    redirect_to group_users_path
+    respond_to do |format|
+      if @group_user.destroy
+        format.html { redirect_to group_users_path }
+        format.js {}
+      else
+        format.js { render :destroy_fail }
+        format.html do
+          redirect_to group_users_path, alert: "Can't remove user from group." \
+            'A user must belong to at least one group.'
+        end
+      end
+    end
   end
 
   private
@@ -18,5 +30,10 @@ class GroupUsersController < ApplicationController
   def set_group
     @group = Group.find_by(name: params[:name])
     redirect_to groups_path, alert: 'Group not found.' unless @group
+  end
+
+  def set_group_user
+    @group_user = GroupUser.find_by(id: params[:id])
+    redirect_to group_users_path, alert: 'User not found.' unless @group_user
   end
 end
--- spec/controllers/group_users_controller_spec.rb
+++ spec/controllers/group_users_controller_spec.rb
@@ -29,10 +29,36 @@ RSpec.describe GroupUsersController, type: :controller do
   describe 'DELETE #destroy' do
     before { delete :destroy, name: group.name }
 
-    it { expect(response).to be_redirect }
-
     it 'sets @group' do
       expect(assigns[:group]).to eq(group)
+    end
+
+    it { expect(response).to be_redirect }
+
+    context 'removing user w/ multiple groups from a group' do
+      let(:group_user) { GroupUser.create(user: user, group: group) }
+
+      before { delete :destroy, name: group.name, id: group_user.id }
+
+      it 'removes user from group' do
+        expect(user.groups).not_to include(group)
+      end
+    end
+
+    context 'removing user w/ only 1 group' do
+      let(:group) { user.groups.first }
+      let(:group_user) { GroupUser.where(user: user, group: group).first }
+
+      before { delete :destroy, name: group.name, id: group_user.id }
+
+      it 'does not remove group' do
+        expect(user.groups).to include(group)
+      end
+
+      it 'sets alert' do
+        expect(flash[:alert]).to eq("Can't remove user from group." \
+          'A user must belong to at least one group.')
+      end
     end
   end
 end
