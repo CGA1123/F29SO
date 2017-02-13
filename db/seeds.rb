19e574ee1069034e131eacd68e04b2dd21f4c5ae
Christian Gregg <c_arlt@hotmail.com>
2017-02-13 17:46:47 +0000

    Update explanatory comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Load in seed files from /db/seeds/*.rb (in lexicographic order)
+# Load in seed files from /db/seeds/*.rb (in lexicographic order, means we can
+# choose what gets created in the db first, important for relations).
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed }
