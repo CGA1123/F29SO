7b0b5da6e84e8bdbaa1da4dcb367c01c958d89b6
Christian Gregg <c_arlt@hotmail.com>
2017-02-17 17:55:25 +0000

    Check for permissions in `AdminPanelController`

--- spec/controllers/admin_panel_controller_spec.rb
+++ spec/controllers/admin_panel_controller_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 RSpec.describe AdminPanelController, type: :controller do
   let(:no_permissions) { FactoryGirl.create(:user) }
+  let(:root_user) { FactoryGirl.create(:root_user) }
+
   describe 'GET #home' do
-    before do
-      sign_in no_permissions
-      get :home
+    context 'no permissions' do
+      before { sign_in no_permissions }
+
+      it_behaves_like 'no permission' do
+        let(:req) { { method: :get, action: :home, params: {} } }
+      end
     end
 
-    it { expect(response).to be_success }
+    context 'has permission' do
+      before do
+        sign_in root_user
+        get :home
+      end
+
+      it { expect(response).to be_success }
+    end
   end
 end
