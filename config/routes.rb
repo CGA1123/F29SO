fce32f282c522d879f2e4123fbb97fb265e5b90a
Christian Gregg <c_arlt@hotmail.com>
2017-02-18 16:30:11 +0000

    Create route, add specs

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,7 @@ Rails.application.routes.draw do
 
   # AdminPanelController Routes
   get 'admin', to: 'admin_panel#home', as: :admin_home
+
+  # NotificationsController Routes
+  get 'notifications', to: 'notifications#index', as: :notifications
 end
--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe NotificationsController, type: :controller do
+  let(:no_permission) { FactoryGirl.create(:user) }
+
+  before { sign_in no_permission }
+
+  describe 'GET #index' do
+    before { get :index, format: :json }
+
+    it { expect(response).to be_success }
+
+    it 'sets @notifications' do
+      expect(assigns[:notifications]).to \
+        eq(Notification.where(recipient: no_permission, read_at: nil))
+    end
+  end
 end
