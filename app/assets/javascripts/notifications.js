3fdf826939c3bfab13f658d12218e2ef30f3ad4b
Christian Gregg <c_arlt@hotmail.com>
2017-02-19 11:24:24 +0000

    Create ajax call and success handler for getting notifications

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var getNotifications = function () {
+  $.ajax({
+    url: '/notifications.json',
+    method: 'GET',
+    dataType: 'JSON'
+  }).success( function(data) { showNotifications(data); } );
+};
+
+var showNotifications = function(data) {
+  console.log(data);
+};
