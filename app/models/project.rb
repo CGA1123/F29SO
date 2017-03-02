0cd837bfbb49cf789175d4e396313b761602af71
Christian Gregg <c_arlt@hotmail.com>
2017-03-02 14:26:06 +0000

    Add associations for ProjectLocation, Location, & Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,14 @@
 class Project < ActiveRecord::Base
+  belongs_to :project_type
+
   has_many :project_groups, dependent: :delete_all
   has_many :users, through: :project_groups
-  belongs_to :project_type
+
   has_many :project_skills, dependent: :destroy
   has_many :skills, through: :project_skills
+
+  has_many :project_locations, dependent: :destroy
+  has_many :locations, through: :project_locations
 
   validates :name, :code, :project_type, presence: true
   validates :code, uniqueness: { case_sensitive: false }
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Location, type: :model do
   it { is_expected.to have_many(:users) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  it { is_expected.to have_many(:project_locations) }
+  it { is_expected.to have_many(:projects).through(:project_locations) }
 end
