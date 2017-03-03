4704da4588dac7cddea3b07c01a8bf8e2608fc28
Christian Gregg <c_arlt@hotmail.com>
2017-03-03 21:14:01 +0000

    Add permission checking for Groups controllers, and admin panel

--- app/controllers/permission_controller.rb
+++ app/controllers/permission_controller.rb
@@ -6,9 +6,11 @@ class PermissionController < ApplicationController
   end
 
   def check_admin_panel
+    not_found unless current_user.permission?('admin.panel')
   end
 
   def check_projects
+
   end
 
   def check_project_groups
@@ -21,12 +23,40 @@ class PermissionController < ApplicationController
   end
 
   def check_groups
+    case action_name
+    when 'index', 'show'
+      not_found unless current_user.permission?('admin.groups.view')
+    when 'create', 'destroy'
+      not_found unless current_user.permission?('admin.groups.manage')
+    else
+      not_found
+    end
   end
 
   def check_group_users
+    case action_name
+    when 'index'
+      not_found unless current_user.permission?('admin.groups.view')
+    when 'create', 'destroy', 'search'
+      not_found unless current_user.permission?('admin.groups.manage.users') || \
+                       current_user.permission?('admin.groups.manage')
+    else
+      not_found
+    end
   end
 
   def check_group_permissions
+    case action_name
+    when 'create', 'destroy'
+      user = current_user
+      not_found unless \
+        user.permission?('admin.groups.manage.permission') \
+        || user.permission?('admin.groups.manage')
+    when 'index'
+      not_found unless current_user.permission?('admin.groups.view')
+    else
+      not_found
+    end
   end
 
   def check_profiles
