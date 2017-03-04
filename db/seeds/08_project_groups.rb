e46f9b8a090d40a966ce7b732f47d20c5c451a21
JAPTaylor <jat1@hw.ac.uk>
2017-03-04 21:05:23 +0000

    Views
    Made some changes to views.
    Added project_group_user seed.
    Still need to fix delete test.
    Still want to make some more changes before meging.

--- db/seeds/08_project_groups.rb
+++ db/seeds/08_project_groups.rb
@@ -4,3 +4,6 @@ ProjectGroup.create!(name: 'Leader', description: 'Directs the operation.', proj
 
 #project_002_groups
 ProjectGroup.create!(name: 'project_leader', description: 'Directs the operation.', project: Project.find_by(name: 'interdimensional_travel'))
+
+#001_users
+ProjectGroupUser.create!(user: User.find_by(email: 'rick@earth-c137.local'), project_group: ProjectGroup.find_by(name: 'smuggler', project: Project.find_by(name: 'seed_excursion')))
