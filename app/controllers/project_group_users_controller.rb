c2da63590aeaeb233921827b906d007033dad2cb
Christian Gregg <c_arlt@hotmail.com>
2017-03-08 13:58:24 +0000

    Remove unecessary render call

--- app/controllers/project_group_users_controller.rb
+++ app/controllers/project_group_users_controller.rb
@@ -29,7 +29,6 @@ class ProjectGroupUsersController < PermissionController
   def search
     string = params[:user]
     @results = User.search(string) unless string.blank?
-    render 'project_group_users/search'
   end
 
   private
