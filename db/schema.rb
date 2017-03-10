559d8d1c9c3a1457476cacbf498ba43b3fb8f479
JAPTaylor <jat1@hw.ac.uk>
2017-03-10 12:45:51 +0000

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -90,36 +90,6 @@ ActiveRecord::Schema.define(version: 20170310115206) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "project_group_permissions", force: :cascade do |t|
-    t.integer  "project_group_id"
-    t.integer  "permission_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  add_index "project_group_permissions", ["permission_id"], name: "index_project_group_permissions_on_permission_id", using: :btree
-  add_index "project_group_permissions", ["project_group_id"], name: "index_project_group_permissions_on_project_group_id", using: :btree
-
-  create_table "project_group_users", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "project_group_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  add_index "project_group_users", ["project_group_id"], name: "index_project_group_users_on_project_group_id", using: :btree
-  add_index "project_group_users", ["user_id"], name: "index_project_group_users_on_user_id", using: :btree
-
-  create_table "project_groups", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.integer  "project_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "project_groups", ["project_id"], name: "index_project_groups_on_project_id", using: :btree
-
   create_table "project_role_locations", force: :cascade do |t|
     t.integer  "project_role_id"
     t.integer  "location_id"
@@ -249,8 +219,6 @@ ActiveRecord::Schema.define(version: 20170310115206) do
   add_foreign_key "invitations", "users", column: "inviter_id"
   add_foreign_key "notifications", "users", column: "actor_id"
   add_foreign_key "notifications", "users", column: "recipient_id"
-  add_foreign_key "project_group_permissions", "project_groups"
-  add_foreign_key "project_group_users", "project_groups"
   add_foreign_key "project_role_locations", "locations"
   add_foreign_key "project_role_locations", "project_roles"
   add_foreign_key "project_role_permissions", "permissions"
