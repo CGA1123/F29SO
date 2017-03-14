6081e2fdb35d036eb72eadcd6aba756baea6e6b0
Christian Gregg <c_arlt@hotmail.com>
2017-03-14 14:43:34 +0000

    Remove '_' from seed names

--- db/seeds/03_groups.rb
+++ db/seeds/03_groups.rb
@@ -7,8 +7,8 @@ Group.create!(name: 'scientist', description: 'The scientist group, for genius s
 # Human
 Group.create!(name: 'human', description: 'The human group, for average humans')
 
-Group.create!(name: 'Project_Manager', description: 'Project Manager group')
+Group.create!(name: 'Project Manager', description: 'Project Manager group')
 Group.create!(name: 'HR', description: 'HR group')
-Group.create!(name: 'Software_Engineer', description: 'Software Engineer group')
+Group.create!(name: 'Software Engineer', description: 'Software Engineer group')
 Group.create!(name: 'Data Scientist', description: 'Data Science group')
 Group.create!(name: 'IT', description: 'IT group')
