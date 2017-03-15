71c14c51ce07cc75ee0088eab4718598531ded3b
Christian Gregg <c_arlt@hotmail.com>
2017-03-15 10:02:41 +0000

    Add a cheeky little footer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set current commit
+COMMIT = `git log --pretty=format:'%h' -n 1`
