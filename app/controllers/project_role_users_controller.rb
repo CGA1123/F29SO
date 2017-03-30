ca44f7fe90a5e2dd5bc3419d1213031610c73d39
JAPTaylor <jat1@hw.ac.uk>
2017-03-30 18:21:42 +0100

    Recalibrating
    Gets rid of those nasty rubocop complaints.
    Fixes some errors.

--- app/controllers/project_role_users_controller.rb
+++ app/controllers/project_role_users_controller.rb
@@ -78,83 +78,6 @@ class ProjectRoleUsersController < PermissionController
   # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
   # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
-  def match
-    # get list of users that are at at least one of the locations
-    skills = @project_role.skills
-
-    users_with_locations = User.where(location: @project_role.locations)
-
-    # get list of users that possess each skill
-    user_ranking = {}
-    users_with_locations.each do |user|
-      # if no skills have been specified in the project role,
-      # rank users based on all the skills they have, else rank them on
-      # the relevant skills
-      if skills.present?
-        intersection = UserSkill.where(user: user, skill: user.skills & skills)
-      else
-        intersection = UserSkill.where(user: user, skill: user.skills)
-      end
-
-      # magically rank them
-      user_ranking[user.id] = rank(intersection) unless intersection.empty?
-    end
-
-    # sort by rank (magic also)
-    ranks = user_ranking.keys.sort_by { |id| -user_ranking[id] }
-    @ranks = []
-    ranks.each do |id|
-      @ranks << User.find(id)
-    end
-  end
-
-  def match
-    # get list of users that are at at least one of the locations
-    users_with_locations = User.where(location: @project_role.locations)
-
-    # get list of users that possess each skill
-    user_ranking = {}
-    users_with_locations.each do |user|
-      intersection = UserSkill.where(user: user,
-                                     skill: @project_role.skills)
-      # magically rank them
-      user_ranking[user.id] = rank(intersection) unless intersection.empty?
-    end
-
-    # sort by rank (magic also)
-    user_ranking.keys.sort_by { |id| -user_ranking[id] }
-  end
-
-  def match
-    # get list of users that are at at least one of the locations
-    locations = @project_role.locations
-    skills = @project_role.skills
-    users_with_locations = User.where(location: locations)
-
-    # get list of users that possess each skill
-    user_ranking = {}
-    users_with_locations.each do |user|
-      # if no skills have been specified in the project role,
-      # rank users based on all the skills they have, else rank them on
-      # the relevant skills
-      if skills.present?
-        intersection = UserSkill.where(user: user, skill: user.skills & skills)
-      else
-        intersection = UserSkill.where(user: user, skill: user.skills)
-      end
-
-      # magically rank them
-      user_ranking[user.id] = rank(intersection) unless intersection.empty?
-    end
-
-    # sort by rank (magic also)
-    ranks = user_ranking.keys.sort_by { |id| -user_ranking[id] }
-    @ranks = []
-    ranks.each do |id|
-      @ranks << User.find(id)
-    end
-  end
-
   private
 
   def set_project
@@ -166,27 +89,13 @@ class ProjectRoleUsersController < PermissionController
   def set_project_role
     @project_role = ProjectRole.find_by(project: @project,
                                         name: params[:name])
-    return if @project_role
-    flash[:alert] = 'Project role not found'
-    redirect_to project_roles_path(code: @project.code)
+    head(404) unless @project_role
   end
 
   def set_project_role_user
     @project_role_user = ProjectRoleUser.find_by(project_role: @project_role,
                                                  user_id: params[:id])
-    return if @project_role_user
-    flash[:alert] = 'Project group user not found'
-    redirect_to project_role_users_path(code: @project.code,
-                                        name: @project_role.name)
-  end
-
-  def rank(skills)
-    rank = skills.count
-    skills.each do |skill|
-      # placeholder. Need to work out the best way to factor in skill rating
-      rank += (skill[:rating] + 1) / 5
-    end
-    rank
+    head(404) unless @project_role_user
   end
 
   def rank(skills)
@@ -200,9 +109,7 @@ class ProjectRoleUsersController < PermissionController
 
   def set_user
     @user = User.find_by(id: params[:id])
-    return if @user
-    flash[:alert] = 'User not found'
-    redirect_to project_path(code: @project.code)
+    head(404) unless @user
   end
 
   def check_format
